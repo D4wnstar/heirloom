e74195b76b9e51d6e2885f8fa7f0e830112dda79
D4wnstar <samvignoli@proton.me>
2026-04-16 14:35:47 +0200

    Added moved href resolution to converter.ts

--- src/lib/converter/converter.ts
+++ src/lib/converter/converter.ts
@@ -22,7 +22,7 @@ import remarkGfm from 'remark-gfm'
 import remarkHighlights from './remark-highlights'
 import remarkComments from './remark-comments'
 import remarkCallouts from './remark-callouts'
-import remarkWikilinks from './remark-wikilinks'
+import remarkWikilinks, { type HrefResolver } from './remark-wikilinks'
 import remarkHeadingIds from './remark-heading-ids'
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkMath from 'remark-math'
@@ -34,6 +34,44 @@ import { matter } from 'vfile-matter'
 import type { Frontmatter } from '$lib/types'
 
 export function markdownToHtml(markdown: string, pathsToRoutes: Record<string, string>) {
+	const hrefResolver: HrefResolver = (target, section) => {
+		if (target) {
+			// The target is either a file name or a file path
+			// If it's a path, we just get the corresponding slug
+			// If it's a name (stem, without ext), we can't be sure if it's unique or not
+			// In this case, we get the first path whose name is equal to the
+			// given name. This check also needs to be case-insensitive
+			// TODO: Check that / is always the correct path separator
+			let path: string | undefined
+			if (target.includes('/')) {
+				path = target
+			} else {
+				path = Object.keys(pathsToRoutes).find((path) => {
+					const fileStem = path.split('/').at(-1)?.replace(/\.md$/, '')
+					return fileStem?.toLowerCase() === target.toLowerCase()
+				})
+			}
+			if (!path) {
+				return null // Probably unintended
+			}
+
+			const route = pathsToRoutes[path]
+			if (!route) {
+				return null // Possibly intended (e.g., link to a future page)
+			}
+
+			return section ? `${route}#${section}` : route
+		} else if (section) {
+			// This is an internal #Section link
+			return '#' + section
+		} else {
+			console.warn(
+				`Detected wikilink with neither target nor section. At least one must be present. Ignoring link`
+			)
+			return null
+		}
+	}
+
 	const processor = unified()
 		.use(remarkParse)
 		.use(remarkFrontmatter, { type: 'yaml', marker: '-' })
@@ -42,7 +80,7 @@ export function markdownToHtml(markdown: string, pathsToRoutes: Record<string, s
 		.use(remarkHighlights)
 		.use(remarkComments)
 		.use(remarkCallouts)
-		.use(remarkWikilinks, { pathsToRoutes })
+		.use(remarkWikilinks, { hrefResolver })
 		.use(remarkHeadingIds)
 		.use(remarkMath)
 		.use(remarkRehype, { allowDangerousHtml: true })
