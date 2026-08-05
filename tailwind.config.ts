3292777ba12f892564f092cff23970d6ee7659dd
D4wnstar <samvignoli@proton.me>
2026-08-06 00:48:44 +0200

    Cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,36 +2,4 @@ import type { Config } from 'tailwindcss'
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}']
-
-	// safelist: [
-	// 	{
-	// 		pattern: /h[1-6]/
-	// 	},
-	// 	{
-	// 		pattern: /preset-filled-\w+-\d+-\d+/
-	// 	},
-	// 	'code-highlight',
-	// 	'code-line',
-	// 	'inserted',
-	// 	'deleted',
-	// 	'highlight-line',
-	// 	'line-number',
-	// 	'callout',
-	// 	'callout-title',
-	// 	'callout-title-inner',
-	// 	'callout-icon',
-	// 	'callout-collapsible',
-	// 	'wrap-break-word',
-	// 	'blockquote',
-	// 	'table-cell',
-	// 	'code',
-	// 	// For lists
-	// 	'list-outside',
-	// 	'list-disc',
-	// 	'list-decimal',
-	// 	'pl-4',
-	// 	'space-y-1',
-	// 	'text-surface-600-400',
-	// 	'checkbox'
-	// ]
 } satisfies Config
